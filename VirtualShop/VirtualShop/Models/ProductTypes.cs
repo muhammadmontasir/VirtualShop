f0ba53987d35ae206ebe90f522f61a02a53d2360
Montasir <m.montasir97@gmail.com>
2020-07-26 15:38:46 +0600

    VirtuaShop file added.

---
+++ VirtualShop/VirtualShop/Models/ProductTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualShop.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Product Type")]
+        public string ProductType { get; set; }
+
+    }
+}
---
+++ VirtualShop/VirtualShop/Models/SpecialTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace VirtualShop.Models
+{
+    public class SpecialTag
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Tag Name")]
+        public string Name { get; set; }
+
+    }
+}
